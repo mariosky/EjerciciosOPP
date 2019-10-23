366fa9ed0df7ec6bb0d9ba4aba8d704813c76351
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-22 22:17:57 -0700

    Domino

--- LopezRoblero/Domino/Program.cs
+++ LopezRoblero/Domino/Program.cs
@@ -2,16 +2,22 @@
 
 namespace Domino
 {
+    //Clase Domino
     class Domino
     {
+        //Espacio 1
         public  int A;
+        //Espacio 2
         public  int B;
-        
+        //Constructor 
         public Domino(int a,int b)
         {
          A=a;
          B=b;
         }
+        //Sobrecarga de operadores(Operador Suma)
+        //Se estan sumando dos piezas de domino
+        //d1(domino 1), d2(domino 2), A(Espacio 1) y B(Espacio 2)
         public static int operator +(Domino d1, Domino d2)
         {
         return  (d1.A+d2.A+d1.B+d2.B);
@@ -21,8 +27,11 @@ namespace Domino
     {
         static void Main(string[] args)
         {
+            //Crear objetos tipo Domino
             Domino a=new Domino (2,0);
             Domino b=new Domino(4,1);
+
+            //Imprimir la suma de los objetos tipo Domino
             Console.WriteLine(a+b);
         }
     }
