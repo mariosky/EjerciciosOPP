e4be8f9e5db9a203c3a0ac319fc8ce8900886018
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-23 19:18:05 -0700

    6

--- LopezRoblero/Figuras/Program.cs
+++ LopezRoblero/Figuras/Program.cs
@@ -3,34 +3,55 @@ using System.Collections.Generic;
 
 namespace Figuras
 {
+    //Clase abstracta Figuta
+    //Abstract en clase: Define clases base, obligando a las clases derivadas a implementar 
+    //los miembros marcados con abstract
     abstract class Figura
     {
+     //Protected: Solo la clase y sus derivadas pueden usar la variable
+     //definida con dicha propiedad
       protected int px,py;
       protected string color;
 
+      //Constructor
       public Figura(int x,int y, string c)
       {
            px=x;
            py=y;
            color=c;
       }
+       //Metodo abstracto dibuja
+        //Abstract en el metodo: Estoy indicando que el metodo no contiene implementacion
+        //y obliga a sus clases derivadas a implementarlos
       public abstract void dibuja();
     }
-    class Rectangulo:Figura
+    //Clase rectangulo que hereda de figura
+     class Rectangulo:Figura
     {
+        //Constructor
+        //base: Manda llamar variables de la clase base(Figura) de la cual estamos heredando
         public Rectangulo(int x, int y, string c):base(x,y,c)
         {
         }
+        //Metodo dibuja
+        //override: Es para implementar metodos de la clase base que marcamos con abstract en esta 
+        //clase derivada
         public override void dibuja()
         {
             Console.WriteLine("Se dibuja rectangulo color {0}, en x:{1} y y:{2}", color,px,py);
         }
     }
+    //Clase rectanculo que hereda de figura
     class Circulo:Figura
     {
+        //Constructor
+        //base: Manda llamar variables de la clase base(Figura) de la cual estamos heredando
         public Circulo(int x, int y, string c):base(x,y,c)
         {
         }
+        //Metodo dibuja
+        //override: Es para implementar metodos de la clase base que marcamos con abstract en esta 
+        //clase derivada
         public override void dibuja()
         {
             Console.WriteLine("Se dibuja circulo color {0}, en x:{1} y y:{2}", color,px,py);
@@ -40,13 +61,16 @@ namespace Figuras
     {
         static void Main(string[] args)
         {
+            //Crear objetos de clases
             Rectangulo r=new Rectangulo(3,5,"Rojo");
             Circulo c=new Circulo(6,10,"Negro");
 
+            //Lista de figuras
             List<Figura> Fig=new List<Figura>();
             Fig.Add(r);
             Fig.Add(c);
 
+            //Estructura repetitiva
             foreach(Figura F in Fig)
             {
                 F.dibuja();
