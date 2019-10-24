c8cd5d9d12a7b8b80b7c832efd393aea5ebf7997
Felix Enrique Leon Molina <felixenriquelm45@gmail.com>
2019-10-23 22:42:00 -0700

    Version 2 "Comentarios"

--- LeonMolinaFelixEnrique/Duracion/Program.cs
+++ LeonMolinaFelixEnrique/Duracion/Program.cs
@@ -2,12 +2,15 @@
 
 namespace Duracion
 {
+    //Clase Duracion 
     class Duracion
     {
+        //Declaramos la variables horas, minutos, segundos con el identificador int en public
         public int horas;
         public int minutos;
         public int segundos;
 
+        //Constructor Duracion
         public Duracion(int h, int m, int s)
         {
             horas = h;
@@ -15,11 +18,13 @@ namespace Duracion
             segundos = s;
         }
 
+        //Constructor Duracion predeterminado
         public Duracion()
         {
 
         }
 
+        //Sobre carga constructor Duracion, convierte segundos a horas y minutos
         public Duracion(int s)
         {
              horas = (int) s/3600;
@@ -27,11 +32,13 @@ namespace Duracion
              segundos = (int) (s-(horas*3600))-(minutos*60);
         }
 
+        //Metodo para imprimir
         public void print()
         {
             Console.WriteLine("{0}:{1}:{2}", horas, minutos, segundos);
         }
 
+        //Metodo segundos para convertir horas y minutos a segundos y sumar todos los segundos
         public void Segundos()
         {
             int totalsegundos;
@@ -39,6 +46,7 @@ namespace Duracion
             Console.WriteLine("Segundos totales:{0}", totalsegundos);
         }
 
+        //Metodo para convertir todo a minutos 
         public void Minutos()
         {
             int totalminutos;
@@ -46,6 +54,7 @@ namespace Duracion
             Console.WriteLine("Minutos totales:{0}\r\n", totalminutos);
         }
 
+        //Operador suma para duracion "a" y duracion "b" 
         public static Duracion operator +(Duracion a, Duracion b)
         {
             int suma=0;
@@ -66,28 +75,38 @@ namespace Duracion
     {
         static void Main(string[] args)
         {
+            //Creacion de objetos tipo duracion 
             Duracion a = new Duracion(2,15,12);
             Duracion b = new Duracion(0,2,15);
             Duracion c = new Duracion(2,0,10);
             Duracion d = new Duracion(7210);
             Duracion e;
+            
+            //Imprimir objeto "a"
             a.print();
+            
+            //Convertir el objeto "a" a segundos y minutos
             a.Segundos();
             a.Minutos();
 
+            //Imprimir el objeto "b" y convertir a segundos y minutos
             b.print();
             b.Segundos();
             b.Minutos();
 
+            //Imprimir el objeto "c" y convertir a segundos y minutos
             c.print();
             c.Segundos();
             c.Minutos();
             
+            //Imprimir objeto "d"
             d.print();
 
+            //Utilizarel operador suma parael objeto "a" y "b" e imprimirlo
             e = a+b;
             e.print();
-            //Console.WriteLine(a+b);
+            
+            /*Console.WriteLine(a+b);*/
         }
     }
 }
