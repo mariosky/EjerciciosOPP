90ea5f41b76f8b634d749f46d9cd38122be20052
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 13:51:19 -0700

    agrege la clase de posgrado

--- TecuapaArturo/Alumno/Program.cs
+++ TecuapaArturo/Alumno/Program.cs
@@ -27,18 +27,28 @@ namespace Alumnos
             return base.Datos_Alumno()+", hago servicio social y recidencia ";
         }    
     }
-  
-   
+    //clase heredada
+    class Posgrado:Alumno{
+        private string pos;
+        //costructor
+        public Posgrado(string nombre, string no_control):base(nombre,no_control){
+                this.pos=pos;
+        }
+        //override para agregar datos al metodo
+        public override string Datos_Alumno(){
+            return base.Datos_Alumno()+", tengo un tema de investigación ";
+        }  
+    }
     class Program
     {
         static void Main(string[] args)
         {
             //crear nuevos objetos en cada clase
             Licenciatura p= new Licenciatura("Jose","12345");
-            
+            Posgrado l= new Posgrado ("Javier","01231");
             //imprimir
             Console.WriteLine(p.Datos_Alumno());
-           
+            Console.WriteLine(l.Datos_Alumno());
           
         }
     }
