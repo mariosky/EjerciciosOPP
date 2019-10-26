a2681cdbc22f08ea7cf16b16afe857039c3e3e51
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 17:45:16 -0700

    cambios

--- TecuapaArturo/Domino/Program.cs
+++ TecuapaArturo/Domino/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Domino
 {
+    class Domino{
+        public int a;
+        public int b;
+        //constructor
+        public Domino(int a, int b){
+            this.a=a;
+            this.b=b;
+        }
+        public static int operator+(Domino a,Domino b){
+            return (a.a+a.b+b.a+b.b);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Domino a= new Domino(2,0);
+            Domino b= new Domino(4,1);
+            Console.WriteLine(a+b);
         }
     }
 }
