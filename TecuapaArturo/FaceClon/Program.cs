dfe8a00fbdbebce3a2228674b53745ebd0025fa7
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 19:23:44 -0700

    cambios

--- TecuapaArturo/FaceClon/Program.cs
+++ TecuapaArturo/FaceClon/Program.cs
@@ -6,28 +6,42 @@ namespace FaceClon
         protected string fecha;
         protected string comentarios;
         protected string reacciones;
-
+        //cosntructor
         public Publicacion(string fecha, string comentarios, string reacciones){
             this.fecha=fecha;
             this.comentarios=comentarios;
             this.reacciones=reacciones;
         }
+        //metodo abstracto que se utilizaran posteriormente en las clases siguientes
         public abstract void imprime();
     }
+    //clases heredadas
     class foto:Publicacion{
         public foto(string fecha, string comentarios, string reacciones):base(fecha,comentarios,reacciones){
 
         }
+        //aqui se utiliza el metodo abstracto
         public override void imprime(){
             Console.WriteLine("La fecha es: {0} \nEl cometario es: {1} \nLa reaccion es: {2}",fecha,comentarios,reacciones);
         }        
+    }
+    class Estado:Publicacion{
+        public Estado(string fecha, string comentarios, string reacciones):base(fecha,comentarios,reacciones){
+            
+        }
+        //aqui se utiliza el metodo abstracto
+        public override void imprime(){
+            Console.WriteLine("La fecha de publicacion: {0} \nEl cometario es: {1} \nLa reaccion es: {2}",fecha,comentarios,reacciones);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             foto f= new foto("12/10/14","Que bueno que esten bien","Me asombra");
+            Estado e=new Estado("21/03/19","Que cool foto","Me divierte");
             f.imprime();
+            e.imprime();
             
         }
     }
