220990341b40be06735e83377b1fae9f06f80d06
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 19:14:24 -0700

    avance

--- TecuapaArturo/FaceClon/Program.cs
+++ TecuapaArturo/FaceClon/Program.cs
@@ -2,6 +2,18 @@
 
 namespace FaceClon
 {
+    class Publicacion{
+        protected string fecha;
+        protected string comentarios;
+        protected string reacciones;
+
+        public Publicacion(string fecha, string comentarios, string reacciones){
+            this.fecha=fecha;
+            this.comentarios=comentarios;
+            this.reacciones=reacciones;
+        }
+        public abstract void imprime();
+    }
     class Program
     {
         static void Main(string[] args)
