cc15e2a94b36df9e3f0e661ee55632fdcf7e5f1f
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 19:13:04 -0700

    nuevo ejercicio

---
+++ TecuapaArturo/FaceClon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaceClon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
