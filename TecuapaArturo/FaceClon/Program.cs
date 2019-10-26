b3b78f4b41045d6b184beaff77dae9bdf520d7b6
ArturoTecuapa50 <54416941+ArturoTecuapa50@users.noreply.github.com>
2019-10-25 19:18:39 -0700

    cambios

--- TecuapaArturo/FaceClon/Program.cs
+++ TecuapaArturo/FaceClon/Program.cs
@@ -2,7 +2,7 @@
 
 namespace FaceClon
 {
-    class Publicacion{
+    abstract class Publicacion{
         protected string fecha;
         protected string comentarios;
         protected string reacciones;
@@ -14,11 +14,21 @@ namespace FaceClon
         }
         public abstract void imprime();
     }
+    class foto:Publicacion{
+        public foto(string fecha, string comentarios, string reacciones):base(fecha,comentarios,reacciones){
+
+        }
+        public override void imprime(){
+            Console.WriteLine("La fecha es: {0} \nEl cometario es: {1} \nLa reaccion es: {2}",fecha,comentarios,reacciones);
+        }        
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foto f= new foto("12/10/14","Que bueno que esten bien","Me asombra");
+            f.imprime();
+            
         }
     }
 }
